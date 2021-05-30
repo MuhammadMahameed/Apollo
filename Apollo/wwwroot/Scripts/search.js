bbf877494e309c024775ab9c999c5960f8e52f66
Zethus <ravet.zvika93@gmail.com>
2021-05-30 16:56:30 +0300

    search artists

---
+++ Apollo/wwwroot/Scripts/search.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    $(".card").hide();
+
+    $("#searchBox").on("input", function () {
+        var str = $("#searchBox").val();
+
+        if (str == "") {
+            $("#apolloIcon").show();
+            $(".card").hide()
+        } else {
+            $("#apolloIcon").hide();
+            $(".stageName").each(function () {
+                if ($(this).text().toUpperCase().match(str.toUpperCase())) {
+                    $(this).parent().parent().show(200);
+                } else {
+                    $(this).parent().parent().hide(200);
+                }
+            });
+        }
+    });
+});
