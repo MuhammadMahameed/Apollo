2f7b99f194feb835938c4142a49a262c6049cacc
Zethus <ravet.zvika93@gmail.com>
2021-05-31 08:18:07 +0300

    added albums and songs to search

--- Apollo/wwwroot/Scripts/search.js
+++ Apollo/wwwroot/Scripts/search.js
@@ -1,5 +1,6 @@
 ﻿$(document).ready(function () {
     $(".card").hide();
+    $("hr").hide()
 
     $("#searchBox").on("input", function () {
         var str = $("#searchBox").val();
@@ -7,11 +8,13 @@
         if (str == "") {
             $("#apolloIcon").show();
             $(".card").hide()
+            $("hr").hide()
         } else {
             $("#apolloIcon").hide();
             $(".stageName").each(function () {
                 if ($(this).text().toUpperCase().match(str.toUpperCase())) {
                     $(this).parent().parent().show(200);
+                    $("hr").show()
                 } else {
                     $(this).parent().parent().hide(200);
                 }
