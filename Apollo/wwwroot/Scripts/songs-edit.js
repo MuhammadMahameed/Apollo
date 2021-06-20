421e01da9d6419adcff90162b73505765fe7975c
Zethus <ravet.zvika93@gmail.com>
2021-06-20 17:59:43 +0300

    on song edit, song's album will display if any exists and available albums will change according to the artist and category

---
+++ Apollo/wwwroot/Scripts/songs-edit.js
@@ -0,0 +1,44 @@
+﻿function getAjax(url, data) {
+    return $.ajax(url, {
+        method: "GET",
+        data: data
+    });
+}
+
+async function getAlbumsByCategory(categoryId, artistId) {
+    matchingAlbums = await getAjax('/Albums/FilterAlbumsByCategoryAndArtist', {
+        categoryId: categoryId,
+        artistId: artistId
+    });
+    return matchingAlbums;
+}
+
+function setAlbumDropDownListValues(categoryId, artistId) {
+    $("#Album").html("<option value=" + 0 + " >N/A</option>")
+
+    getAlbumsByCategory(categoryId, artistId).then((data) => {
+        data.$values.forEach((record) => {
+            $("#Album").append("<option value=" + record.id + " >" + record.title + "</option>");
+        });
+    });
+}
+
+// on page start
+$(document).ready(function () {
+    var categoryId = $("#Category").val();
+    var artistId = $("#Artist").val();
+});
+
+// on category change
+$("#Category").on('input', function (e) {
+    var categoryId = $("#Category").val();
+    var artistId = $("#Artist").val();
+    setAlbumDropDownListValues(categoryId, artistId);
+});
+
+// on artist change
+$("#Artist").on('input', function (e) {
+    var categoryId = $("#Category").val();
+    var artistId = $("#Artist").val();
+    setAlbumDropDownListValues(categoryId, artistId);
+});
