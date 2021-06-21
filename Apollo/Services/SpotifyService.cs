ae594c3768e96867fac8fa4bda0c935bf315929a
Zethus <ravet.zvika93@gmail.com>
2021-06-22 01:51:46 +0300

    new updates page displaying spotify newly released songs

---
+++ Apollo/Services/SpotifyService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Apollo.Models;
+
+namespace Apollo.Services
+{
+    public class SpotifyService
+    {
+        private readonly HttpClient _httpClient;
+        public SpotifyService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> GetToken(string client_id, string client_secret)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, "token");
+
+            request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(
+                "Basic", Convert.ToBase64String(Encoding.UTF8.GetBytes($"{client_id}:{client_secret}")));
+
+            request.Content = new FormUrlEncodedContent(new Dictionary<string, string>
+            {
+                {"grant_type", "client_credentials" }
+            });
+
+            var response = await _httpClient.SendAsync(request);
+
+            response.EnsureSuccessStatusCode();
+
+            using var responseStream = await response.Content.ReadAsStreamAsync();
+            var authResult = await JsonSerializer.DeserializeAsync<SpotifyAuthResult>(responseStream);
+
+            return authResult.access_token;
+        }
+    }
+}
---
+++ Apollo/wwwroot/Scripts/updates.js
@@ -0,0 +1,35 @@
+﻿function getAjax(url, data) {
+    return $.ajax(url, {
+        method: "GET",
+        data: data
+    });
+}
+
+async function getMatchingSongs(limit) {
+    var updates = await getAjax('/Updates/GetReleases', { limit: limit });
+    return updates.$values;
+}
+
+$(document).ready(function () {
+    var limit = 20;
+    getMatchingSongs(limit).then((data) => {
+        console.log(data);
+        var numCardsPerRow = 5
+
+        for (var i = 0; i < data.length; i++) {
+            if (i % numCardsPerRow == 0) {
+                $("#updates").append('<div class="row row' + parseInt(i / numCardsPerRow) + ' d-flex justify-content-center"></div>');
+            }
+
+            var template = '<div class="card"><div class="card-body center">' +
+                '<a href="' + data[i].link + '">' +
+                '<img class="card-img-top" src=' + data[i].imageUrl + ' alt="Card image cap">' +
+                '<h5 class="card-title">' + data[i].name + '</h5>' +
+                "</a>" +
+                '<p>' + data[i].artists + '</p>' +
+                '<p>' + data[i].date + '</p>' +
+                '</div></div>'
+            $("#updates .row" + parseInt(i / numCardsPerRow)).append(template);
+        }
+    })
+});
