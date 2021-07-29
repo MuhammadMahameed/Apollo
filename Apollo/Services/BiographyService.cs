b0a2c0d314de585bdac3ec1ebfef66c7c7fa9092
Zethus <ravet.zvika93@gmail.com>
2021-07-29 11:13:33 +0300

    added search to biographies

--- Apollo/Services/BiographyService.cs
+++ Apollo/Services/BiographyService.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Apollo.Data;
+using Apollo.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Apollo.Services
 {
@@ -12,6 +15,41 @@ namespace Apollo.Services
         public BiographyService(DataContext context)
         {
             _context = context;
+        }
+
+        public ArrayList FilterBiographies(string str)
+        {
+            var strToLower = str.ToLower();
+
+            var matchingBiographies = _context.Biography
+                .Include(x => x.Artist)
+                .Where(s => s.Artist.StageName.ToLower().Contains(strToLower) ||
+                            s.EarlyLife.ToLower().Contains(strToLower) ||
+                            s.Career.ToLower().Contains(strToLower) ||
+                            s.Artistry.ToLower().Contains(strToLower) ||
+                            s.PersonalLife.ToLower().Contains(strToLower) ||
+                            s.NumberOfSongs.ToString().ToLower().Contains(strToLower) ||
+                            s.NumberOfAlbums.ToString().ToLower().Contains(strToLower))
+                .ToList();
+
+            ArrayList matchingBiographiesList = new ArrayList();
+
+            foreach (Biography biography in matchingBiographies)
+            {
+                matchingBiographiesList.Add(new
+                {
+                    id = biography.Id,
+                    artistStageName = biography.Artist.StageName,
+                    earlyLife = biography.EarlyLife,
+                    career = biography.Career,
+                    artistry = biography.Artistry,
+                    personalLife = biography.PersonalLife,
+                    numberOfSongs = biography.NumberOfSongs,
+                    numberOfAlbums = biography.NumberOfAlbums
+                });
+            }
+
+            return matchingBiographiesList;
         }
     }
 }
---
+++ Apollo/wwwroot/Scripts/biographies.js
@@ -0,0 +1,58 @@
+﻿$(document).ready(function () {
+});
+
+function getAjax(url, data) {
+    return $.ajax(url, {
+        method: "GET",
+        data: data
+    });
+}
+
+async function getMatchingBiographies(matchingStr) {
+    matchingBiographies = await getAjax('/Biographies/Filter', { matchingStr: matchingStr });
+    return matchingBiographies;
+}
+
+async function getAllBiographies() {
+    allBiographies = await getAjax('/Biographies/Index', {});
+    return allBiographies
+}
+
+$("#searchBox").on('input', function (e) {
+    var matchingStr = $("#searchBox").val();
+    $("table tbody").html("");
+    $("#noData").html("");
+
+    if (matchingStr) {
+        getMatchingBiographies(matchingStr).then((data) => {
+            if (data.$values.length == 0)
+                $("#noData").append('<img src="Assets/nothing_found.png">');
+
+            data.$values.forEach(record => {
+                var row = "<tr>" + 
+                    "<td>" + record.artistStageName + "</td>" +
+                    "<td>" + record.earlyLife + "</td>" +
+                    "<td>" + record.career + "</td>" +
+                    "<td>" + record.artistry + "</td>" +
+                    "<td>" + record.personalLife + "</td>" +
+                    "<td>" + record.numberOfSongs + "</td>" +
+                    "<td>" + record.numberOfAlbums + "</td>" + "<td>" +
+                    "<a href=\"/Branches/Edit/" + record.id + "\">Edit</a>" + " | " +
+                    "<a href=\"/Branches/Details/" + record.id + "\">Details</a>" + " | " +
+                    "<a href=\"/Branches/Delete/" + record.id + "\">Delete</a>" +
+                    "</td></tr>"
+
+                $("table tbody").append(row);
+            });
+        });
+    } else {
+        getAllBiographies().then((data) => {
+            var rows = $(data).find("table tbody tr");
+            $("#noData").html("");
+            $("table tbody").html("");
+            $("table tbody").html(rows);
+            if (rows.length == 0)
+                $("#noData").append('<img src="Assets/nothing_found.png">');
+        });
+    }
+});
