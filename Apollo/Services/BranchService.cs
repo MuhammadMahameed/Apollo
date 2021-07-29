828cd5d262a1c79b859c2e7c72b9578805346574
Zethus <ravet.zvika93@gmail.com>
2021-07-29 10:49:48 +0300

    added search to branches page

--- Apollo/Services/BranchService.cs
+++ Apollo/Services/BranchService.cs
@@ -1,18 +1,23 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Apollo.Data;
+using Apollo.Models;
 using Microsoft.Extensions.Configuration;
 
 namespace Apollo.Services
 {
     public class BranchService
     {
+        private readonly DataContext _context;
         private readonly IConfiguration _configuration;
-        public BranchService(IConfiguration configuration)
+        public BranchService(IConfiguration configuration, DataContext context)
         {
             _configuration = configuration;
+            _context = context;
         }
 
         public async Task<byte[]> GetBingMap(string coordinates)
@@ -23,6 +28,30 @@ namespace Apollo.Services
             HttpResponseMessage responseMap = await httpClient.SendAsync(requestMap);
             var stream = await responseMap.Content.ReadAsByteArrayAsync();
             return stream;
+        }
+
+        public ArrayList FilterBranches(string str)
+        {
+            var strToLower = str.ToLower();
+
+            var matchingBranches = _context.Branch
+                .Where(s => s.AddressName.ToLower().Contains(strToLower) ||
+                            s.Coordinate.ToLower().Contains(strToLower))
+                .ToList();
+
+            ArrayList matchingBranchesList = new ArrayList();
+
+            foreach (Branch branch in matchingBranches)
+            {
+                matchingBranchesList.Add(new
+                {
+                    id = branch.Id,
+                    addressName = branch.AddressName,
+                    coordinate = branch.Coordinate
+                });
+            }
+
+            return matchingBranchesList;
         }
     }
 }
