01cdda79405e9ce137e2026612b1135377365262
Zethus <ravet.zvika93@gmail.com>
2021-07-29 11:36:22 +0300

    added search to labels page

--- Apollo/Services/LabelService.cs
+++ Apollo/Services/LabelService.cs
@@ -32,5 +32,36 @@ namespace Apollo.Services
 
             return labels;
         }
+
+        public ArrayList FilterLabels(string str)
+        {
+            var strToLower = str.ToLower();
+
+            var matchingLabels = _context.Label
+                .Include(x => x.Artists)
+                .Where(s => s.Name.ToLower().Contains(str) ||
+                            s.Status.ToLower().Contains(str) ||
+                            s.Country.ToLower().Contains(str) ||
+                            s.Founded.ToString().ToLower().Contains(str) ||
+                            s.Artists.Select(x => x.StageName).Contains(str))
+                .ToList();
+
+            ArrayList matchingLabelsList = new ArrayList();
+
+            foreach (Label label in matchingLabels)
+            {
+                matchingLabelsList.Add(new
+                {
+                    id = label.Id,
+                    name = label.Name,
+                    status = label.Status,
+                    country = label.Country,
+                    founded = label.Founded,
+                    artists = label.Artists.Select(x => x.StageName)
+                });
+            }
+
+            return matchingLabelsList;
+        }
     }
 }
